c94081920d22cb75c02b97ea20ca5a07d042cc81
Kamal Dhital <kdhital143@gmail.com>
2025-03-18 14:59:43 +0545

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.71%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.py
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -1,4 +1,3 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        square_root = int(num ** 0.5)
-        return (square_root * square_root == num)
+        return (int(num**0.5) * int(num**0.5) == num)
